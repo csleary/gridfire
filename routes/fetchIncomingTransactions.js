afc6120107d5c55300f540aeaa9234765964a4d9
Chris <csleary@gmail.com>
2017-12-22 23:52:59 +0000

    Fetch transactions refactoring.

--- routes/fetchIncomingTransactions.js
+++ routes/fetchIncomingTransactions.js
@@ -1,36 +1,35 @@
 module.exports = (paymentAddress, idHash, done) => {
   const nem = require('nem-sdk').default;
+
   const endpoint = nem.model.objects.create('endpoint')(
     process.env.REACT_APP_NEM_NETWORK === 'mainnet'
       ? nem.model.nodes.defaultMainnet
       : nem.model.nodes.defaultTestnet,
     nem.model.nodes.defaultPort
   );
+
   const nemNode = endpoint.host;
   let incomingTxs = [];
   let paidToDate = 0;
   let txId;
 
   // Filter transactions to show only those matching the purchase ID.
-  const filterTransactions = (transactions) => {
-    const filtered = [];
+  const filterTransactions = (transactions, filtered = []) => {
     transactions.forEach((tx) => {
       const { hexMessage } = nem.utils.format;
 
       // Check first for multisig payments, then normal payments.
-      if ('otherTrans' in tx.transaction) {
-        const message = hexMessage({
-          type: 1,
-          payload: tx.transaction.otherTrans.message.payload
-        });
-        if (message === idHash) filtered.push(tx);
-      } else {
-        const message = hexMessage({
-          type: 1,
-          payload: tx.transaction.message.payload
-        });
-        if (message === idHash) filtered.push(tx);
-      }
+      const payload =
+        'otherTrans' in tx.transaction
+          ? tx.transaction.otherTrans.message.payload
+          : tx.transaction.message.payload;
+
+      const message = hexMessage({
+        type: 1,
+        payload
+      });
+
+      if (message === idHash) filtered.push(tx);
     });
     return filtered;
   };
@@ -38,19 +37,17 @@ module.exports = (paymentAddress, idHash, done) => {
   // Total payments, check if price has been met.
   const paid = [];
   const checkPayments = (transactions) => {
-    transactions.forEach((payment) => {
-      if ('otherTrans' in payment.transaction) {
-        paid.push(payment.transaction.otherTrans.amount);
-      } else {
-        paid.push(payment.transaction.amount);
-      }
+    transactions.forEach((tx) => {
+      'otherTrans' in tx.transaction
+        ? paid.push(tx.transaction.otherTrans.amount)
+        : paid.push(tx.transaction.amount);
     });
+
     let sum = paid.reduce((acc, cur) => acc + cur, 0);
     sum *= 10 ** -6;
     return sum;
   };
 
-  // Fetch a list of transactions.
   const fetchTransactions = async (callback) => {
     const tx = await nem.com.requests.account.transactions.incoming(
       endpoint,
