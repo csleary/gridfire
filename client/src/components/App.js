d480c856801b33d91221126e0328c9ba93fdded6
Chris Leary <csleary@users.noreply.github.com>
2018-01-09 09:16:22 +0000

    Dev (#20)
    * Removing scroll event listeners for now.
    
    * Changed navbar to react component, added scroll event listener for dynamic logo.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -20,7 +20,6 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      logoOpacity: 0,
       isLoading: true
     };
   }
@@ -28,29 +27,6 @@ class App extends Component {
   componentDidMount() {
     window.scrollTo(0, 0);
     this.props.fetchUser().then(() => this.setState({ isLoading: false }));
-
-    let y = 0;
-    let ticking = false;
-    window.addEventListener('scroll', () => {
-      y = window.scrollY;
-
-      if (!ticking) {
-        window.requestAnimationFrame(() => {
-          // this.showLogo(y);
-          ticking = false;
-        });
-        ticking = true;
-      }
-    });
-  }
-
-  showLogo(y) {
-    const headerHeight = document.getElementById('header').offsetHeight;
-    if (y > headerHeight) {
-      this.setState({ logoOpacity: 1 });
-    } else {
-      this.setState({ logoOpacity: 0 });
-    }
   }
 
   render() {
@@ -80,7 +56,7 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Header user={this.props.user} />
-          <Navbar user={this.props.user} logoOpacity={this.state.logoOpacity} />
+          <Navbar user={this.props.user} />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
