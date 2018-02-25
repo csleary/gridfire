43d798cfdaa36012e9c67eb239707b282565fbf4
Chris <csleary@gmail.com>
2018-02-25 15:41:53 +0000

    Clean up NEM payment route, convert cbs to promises.

--- routes/nemRoutes.js
+++ routes/nemRoutes.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-// const nem = require('nem-sdk').default;
 const jwt = require('jsonwebtoken');
 const fetchIncomingTransactions = require('./fetchIncomingTransactions');
 const keys = require('../config/keys');
@@ -13,47 +12,46 @@ module.exports = app => {
   app.post('/api/nem/transactions', requireLogin, async (req, res) => {
     const { releaseId, paymentHash } = req.body;
     const { price } = req.session;
+    const user = await User.findById(req.user._id);
     const release = await Release.findById(releaseId);
     const artist = await User.findById(release._user);
     const paymentAddress = artist.nemAddress;
-    const hasPreviouslyPurchased = req.user.purchases.some(
+    const hasPreviouslyPurchased = user.purchases.some(
       purchase => releaseId === purchase.releaseId
     );
 
-    fetchIncomingTransactions(
+    const transactions = await fetchIncomingTransactions(
       paymentAddress,
-      paymentHash,
-      async transactions => {
-        if (transactions.paidToDate >= price || hasPreviouslyPurchased) {
-          // Add purchase to user account, if not already added.
-          if (!hasPreviouslyPurchased) {
-            const user = await User.findById(req.user._id);
-            user.purchases.push({
-              purchaseDate: Date.now(),
-              releaseId
-            });
-            user.save();
-
-            // Increment sales.
-            release.numSold += 1;
-            release.save();
-          }
-
-          // Issue download token to user on successful payment.
-          const token = jwt.sign(
-            {
-              releaseId,
-              expiresIn: '10m'
-            },
-            keys.nemp3Secret
-          );
-          res.append('Authorization', `Bearer ${token}`);
-          res.send(transactions);
-        } else {
-          res.send(transactions);
-        }
-      }
+      paymentHash
     );
+
+    transactions.hasPreviouslyPurchased = hasPreviouslyPurchased;
+
+    if (transactions.paidToDate >= price || hasPreviouslyPurchased) {
+      // Add purchase to user account, if not already added.
+      if (!hasPreviouslyPurchased) {
+        user.purchases.push({
+          purchaseDate: Date.now(),
+          releaseId
+        });
+        user.save();
+
+        // Increment sales.
+        release.numSold += 1;
+        release.save();
+      }
+
+      // Issue download token to user on successful payment.
+      const token = jwt.sign(
+        {
+          releaseId,
+          expiresIn: '10m'
+        },
+        keys.nemp3Secret
+      );
+      res.append('Authorization', `Bearer ${token}`);
+    }
+    res.send(transactions);
   });
 
   app.get('/api/nem/price', async (req, res) => {
