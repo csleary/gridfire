5f2557e903013014745cc41ab6361b31a9fe764e
Chris <csleary@gmail.com>
2018-07-05 15:28:57 +0100

    Clarify manual payment amount.

--- client/src/components/dashboard/Collection.js
+++ client/src/components/dashboard/Collection.js
@@ -57,7 +57,7 @@ class Collection extends Component {
         <main className="container">
           <div className="row">
             <div className="col">
-              <h3>No releases found.</h3>
+              <h3>No releases found</h3>
               <p>
                 Once you&rsquo;ve purchased a release it will be added here,
                 where you&rsquo;ll have easy access to your downloads.
