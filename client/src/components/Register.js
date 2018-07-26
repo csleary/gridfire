18fe6814d14be24b2fdfe7412a5e1b199ccf41a6
Chris <csleary@gmail.com>
2018-07-26 12:20:08 +0100

    Create captcha component.

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -4,15 +4,15 @@ import FontAwesome from 'react-fontawesome';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import Recaptcha from 'react-google-recaptcha';
+import RenderRecaptcha from './RenderRecaptcha';
 import { fetchUser, toastError, toastSuccess } from '../actions';
-
-const sitekey = process.env.REACT_APP_RECAPTCHA_SITE_KEY;
 
 class Register extends Component {
   onSubmit = values => {
+    const captcha = this.captcha;
     this.register(values, () => {
       this.props.reset();
+      captcha.getRenderedComponent().reset();
       this.props.history.push('/');
     });
   };
@@ -30,23 +30,6 @@ class Register extends Component {
   };
 
   required = value => (value ? undefined : 'Please enter a value.');
-
-  renderRecaptcha = field => {
-    const { error, input, touched } = field;
-
-    return (
-      <div className="form-group d-flex flex-wrap justify-content-center py-2">
-        <Recaptcha
-          onChange={response => input.onChange(response)}
-          sitekey={sitekey}
-        />
-        {touched &&
-          error && (
-            <div className="invalid-feedback">{touched && error && error}</div>
-          )}
-      </div>
-    );
-  };
 
   renderField = field => {
     const {
@@ -91,48 +74,54 @@ class Register extends Component {
       <main className="container">
         <div className="row">
           <div className="col">
-            <h2 className="text-center mt-4">Register</h2>
-            <form onSubmit={handleSubmit(this.onSubmit)}>
-              <div className="form-row mt-5">
-                <div className="col-md-6 mx-auto">
-                  <Field
-                    component={this.renderField}
-                    icon="envelope-o"
-                    id="email"
-                    label="Email Address:"
-                    name="email"
-                    placeholder="Email Address"
-                    required
-                    type="email"
-                    validate={this.required}
-                  />
-                  <Field
-                    className="form-control"
-                    component={this.renderField}
-                    hint="A strong and unique alphanumeric password recommended."
-                    icon="key"
-                    id="password"
-                    label="Password:"
-                    name="password"
-                    placeholder="Password"
-                    required
-                    type="password"
-                    validate={this.required}
-                  />
-                  <Field
-                    component={this.renderRecaptcha}
-                    name="recaptcha"
-                    validate={this.required}
-                  />
-                  <div className="d-flex justify-content-center">
-                    <button
-                      className="btn btn-outline-primary"
-                      disabled={invalid || pristine || submitting}
-                      type="submit"
-                    >
-                      Sign Up
-                    </button>
-                  </div>
+            <h2 className="text-center red mt-4">Register</h2>
+            <form
+              className="form-row mt-5"
+              onSubmit={handleSubmit(this.onSubmit)}
+            >
+              <div className="col-md-6 mx-auto">
+                <Field
+                  component={this.renderField}
+                  icon="envelope-o"
+                  id="email"
+                  label="Email Address:"
+                  name="email"
+                  placeholder="Email Address"
+                  required
+                  type="email"
+                  validate={this.required}
+                />
+                <Field
+                  className="form-control"
+                  component={this.renderField}
+                  hint="A strong and unique alphanumeric password recommended."
+                  icon="key"
+                  id="password"
+                  label="Password:"
+                  name="password"
+                  placeholder="Password"
+                  required
+                  type="password"
+                  validate={this.required}
+                />
+                <Field
+                  classNames="justify-content-end"
+                  component={RenderRecaptcha}
+                  name="recaptcha"
+                  ref={el => {
+                    this.captcha = el;
+                  }}
+                  validate={this.required}
+                  withRef
+                />
+                <div className="d-flex justify-content-end">
+                  <button
+                    className="btn btn-outline-primary my-3"
+                    disabled={invalid || pristine || submitting}
+                    type="submit"
+                  >
+                    Sign Up
+                  </button>
                 </div>
               </div>
             </form>
