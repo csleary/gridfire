200fb1c392bad9b63a35e293c89c603fdcfff70a
Chris <csleary@gmail.com>
2018-07-29 11:52:44 +0100

    nextProp check improved to avoid rerender.

--- client/src/components/SelectedRelease.js
+++ client/src/components/SelectedRelease.js
@@ -48,7 +48,9 @@ class SelectedRelease extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.match.params !== this.props.match.params) {
+    if (
+      nextProps.match.params.releaseId !== this.props.match.params.releaseId
+    ) {
       this.setState({ isLoading: true });
       const { releaseId } = nextProps.match.params;
       this.props
