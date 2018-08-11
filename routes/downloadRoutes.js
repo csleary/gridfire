ea50596a66ce2808afdbef6682ff80b1eb9d0dd5
Chris <csleary@gmail.com>
2018-08-11 16:31:59 +0100

    Stream audio into download zip.

--- routes/downloadRoutes.js
+++ routes/downloadRoutes.js
@@ -2,7 +2,6 @@ const archiver = require('archiver');
 const aws = require('aws-sdk');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
-const request = require('request');
 const keys = require('../config/keys');
 const requireLogin = require('../middlewares/requireLogin');
 const { AWS_REGION, BUCKET_IMG, BUCKET_SRC } = require('./constants');
@@ -53,42 +52,34 @@ module.exports = app => {
       .listObjectsV2({ Bucket: BUCKET_SRC, Prefix: prefix })
       .promise();
 
-    const downloadUrls = s3List.Contents.map(s3Track => {
-      const title =
-        process.env.NEM_NETWORK === 'mainnet'
-          ? trackList.filter(track => s3Track.Key.includes(track._id))[0]
-              .trackTitle
-          : 'Test Track';
-
-      const ext = s3Track.Key.substring(s3Track.Key.lastIndexOf('.'));
-
-      const params = {
-        Bucket: BUCKET_SRC,
-        Expires: 60 * 60,
-        Key: s3Track.Key
-      };
-
-      const url = s3.getSignedUrl('getObject', params);
-      return { ext, title, url };
-    });
-
     archive.on('end', () => {});
-
+    archive.on('warning', () => {});
     archive.on('error', () => {});
 
     res.attachment(`${artistName} - ${releaseTitle}.zip`);
     archive.pipe(res);
 
-    downloadUrls.forEach((s3Track, index) => {
+    s3List.Contents.forEach((s3Track, index) => {
+      const { Key } = s3Track;
+
       const trackNumber =
         process.env.NEM_NETWORK === 'mainnet'
-          ? trackList.findIndex(track => s3Track.url.includes(track._id)) + 1
+          ? trackList.findIndex(track => Key.includes(track._id)) + 1
           : index + 1;
 
-      archive.append(request(s3Track.url, { encoding: null }), {
-        name: `${trackNumber.toString(10).padStart(2, '0')} ${s3Track.title}${
-          s3Track.ext
-        }`
+      const title =
+        process.env.NEM_NETWORK === 'mainnet'
+          ? trackList.filter(track => Key.includes(track._id))[0].trackTitle
+          : 'Test Track';
+
+      const ext = Key.substring(Key.lastIndexOf('.'));
+
+      const trackSrc = s3
+        .getObject({ Bucket: BUCKET_SRC, Key })
+        .createReadStream();
+
+      archive.append(trackSrc, {
+        name: `${trackNumber.toString(10).padStart(2, '0')} ${title}${ext}`
       });
     });
 
