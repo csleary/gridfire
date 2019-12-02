214a08b4058e1ba355e42ca887a1741d34d15e90
Chris Leary <csleary@users.noreply.github.com>
2019-12-02 09:18:06 +0100

    Dev (#232)
    * Restructure project to more closely resemble component tree, couple styles.
    
    * Add files.
    
    * Custom spinner.

--- client/src/components/spinner/index.js
+++ client/src/components/spinner/index.js
@@ -1,14 +1,13 @@
-import FontAwesome from 'react-fontawesome';
-import PropTypes from 'prop-types';
-import React from 'react';
-import { spinner } from './spinner.module.css';
+import PropTypes from "prop-types";
+import React from "react";
+import { spinner } from "./spinner.module.css";
 
 const Spinner = ({ children }) => (
   <main className="container d-flex align-items-center justify-content-center">
     <div className="row mb-5">
       <div className="col text-center py-3">
         {children}
-        <FontAwesome name="circle-o-notch" spin className={spinner} />
+        <div className={`${spinner}`} />
       </div>
     </div>
   </main>
