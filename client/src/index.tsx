95d614aa61c0e4851a20f1d05bd01dee6dea2313
Christopher Leary <C:\Users\Chris\AppData\Roaming\The Bat! Pwd>
2020-10-25 10:15:57 +0100

    Unregister sw for now.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -25,7 +25,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
 
 export { CLOUD_URL };
 export type RootState = ReturnType<typeof store.getState>;
