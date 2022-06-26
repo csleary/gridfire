b8a734049ec8f72688bec2315f4c81dce6ffc14c
Chris Leary <csleary@gmail.com>
2022-06-26 15:58:40 +0200

    Update ipfs config with gateway cors settings.

--- manifests/production/ipfs/ipfs-config.sh
+++ manifests/production/ipfs/ipfs-config.sh
@@ -3,4 +3,5 @@ set -ex
 echo "Applying custom IPFS configuration…"
 ipfs config profile apply server
 ipfs config Reprovider.Strategy roots
-
+ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Origin '["https://gridfire.app"]'
+ipfs config --json Gateway.HTTPHeaders.Access-Control-Allow-Methods '["GET"]'
